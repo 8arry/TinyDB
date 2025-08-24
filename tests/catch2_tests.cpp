8e3ebdf16dc2a2181113595f3735f2e29a0cf82e
8arry <1173309602@qq.com>
2025-08-24 16:20:38 +0200

    Debug: catch2_tests

--- tests/catch2_tests.cpp
+++ tests/catch2_tests.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-//#include <catch2/catch_test_macros.hpp>
+// #include <catch2/catch_test_macros.hpp>
 // Include project header files
 #include "../libcore/database/condition.hpp"
 #include "../libcore/database/database.hpp"
